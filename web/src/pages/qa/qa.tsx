6ce1a0f3751ed92d0ce738ec81399271b7c4a712
linyh <yanghui@meri.co>
2023-11-03 10:27:35 +0800

    调整ai对话展示的方式

--- web/src/pages/qa/qa.tsx
+++ web/src/pages/qa/qa.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import { useEffect, useRef, useState } from 'react';
-import { Card, Col, Row, Input, Skeleton, Tag, Button, Affix } from 'antd';
+import { useEffect, useState } from 'react';
+import { Button, Card, Col, Input, Row, Skeleton } from 'antd';
 import Frame from '../../utils/frame';
 import Block from '../../utils/block';
 import { getHotQuestions, qa } from '../../apis/qa';
@@ -8,6 +8,7 @@ import robotPng from '../../assets/robot.png';
 import questionPng from '../../assets/question.png';
 import accountManager from '../account/accountManager';
 import useQuery from '../../utils/query';
+
 
 export const QaPage = () => {
 
@@ -20,8 +21,7 @@ export const QaPage = () => {
   const [questions, setQuestions] = useState<string[]>([]);
   const [tags, setTags] = useState<string[]>([]);
 
-  const [qaQuestion, qaQuestionSetter] = React.useState('');
-  const [qaAnswer, qaAnswerSetter] = React.useState('');
+  const [qas, qasSetter] = React.useState<{ type: 'ai' | 'user', message: string }[]>([]);
   const [qaAnswerLoading, qaAnswerLoadingSetter] = React.useState(false);
 
   // 请求热门tags、及参数中tag或者问题的答案
@@ -52,15 +52,15 @@ export const QaPage = () => {
   }
   const requestQA = async(question: string) => {
     questionTextSetter('')
-    qaQuestionSetter(question)
+    qasSetter(qas => [...qas, { type: 'user', message: question }])
 
     guessTagShowSetter(false)
     qaAnswerLoadingSetter(true)
     try {
       const { answer } = await qa(question)
-      qaAnswerSetter(answer)
+      qasSetter(qas => [...qas, { type: 'ai', message: answer }])
     } catch (e) {
-      qaAnswerSetter('服务器连接失败，请稍后再试')
+      qasSetter(qas => [...qas, { type: 'ai', message: '服务器连接失败，请稍后再试' }])
     } finally {
       qaAnswerLoadingSetter(false)
     }
@@ -71,15 +71,14 @@ export const QaPage = () => {
       <Block padding style={{ paddingTop: '20px', paddingBottom: 0 }}>
         <Row>
           <Col span={3}>
-            <img src={robotPng} style={{ width: '80%', height: 'auto' }} />
+            <img src={robotPng} style={{ width: '80%', height: 'auto' }} alt="robot" />
           </Col>
           <Col span={18} style={{ lineHeight: '100px', fontSize: '40px', textAlign: 'left', }}>
             <span>小Ai</span>
           </Col>
           <Col span={3}>
-            <img src={questionPng}
-                   onClick={() => guessTagShowSetter(true)}
-                   style={{ width: '80%', height: 'auto' }} />
+            <img src={questionPng} onClick={() => guessTagShowSetter(true)}
+                 style={{ width: '80%', height: 'auto' }} alt="guess tag" />
           </Col>
         </Row>
       </Block>
@@ -89,31 +88,29 @@ export const QaPage = () => {
         </Card>
       </Block>
 
-      {!guessTagShow && qaQuestion && <>
-        <Block padding style={{ paddingTop: '20px', paddingBottom: 0 }}>
-          <Row>
-            <Col span={18} offset={3} style={{ lineHeight: '100px', fontSize: '40px', textAlign: 'right' }}>
-              <span style={{ lineHeight: '100px' }}>{account.nickname || '我'}</span>
-            </Col>
-            <Col span={3}>
-              <img src={account.avatar_url} style={{ width: '80%', height: 'auto' }} />
-            </Col>
-          </Row>
-        </Block>
-        <Block padding style={{ padding: '10px', paddingBottom: '10px' }}>
-          <Card style={{ backgroundColor: '#5b83fd', color: 'white' }}>
-            <div>{qaQuestion}</div>
-          </Card>
-        </Block>
-      </>}
-
-      {!guessTagShow && qaQuestion && (qaAnswerLoading
-        ? <Block padding><Skeleton /></Block>
-        : <>
+      {!guessTagShow && qas.map((qa) => <>
+        {qa.type === 'user' && <>
+          <Block padding style={{ paddingTop: '20px', paddingBottom: 0 }}>
+            <Row>
+              <Col span={18} offset={3} style={{ lineHeight: '100px', fontSize: '40px', textAlign: 'right' }}>
+                <span style={{ lineHeight: '100px' }}>{account.nickname || '我'}</span>
+              </Col>
+              <Col span={3}>
+                {account.avatar_url && <img src={account.avatar_url} style={{ width: '80%', height: 'auto' }} alt="my avatar" />}
+              </Col>
+            </Row>
+          </Block>
+          <Block padding style={{ padding: '10px', paddingBottom: '10px' }}>
+            <Card style={{ backgroundColor: '#5b83fd', color: 'white' }}>
+              <div>{qa.message}</div>
+            </Card>
+          </Block>
+        </>}
+        {qa.type === 'ai' && <>
           <Block padding style={{ padding: '20px 10px 0 10px' }}>
             <Row>
               <Col span={3}>
-                <img src={robotPng} style={{ width: '80%', height: 'auto' }} />
+                <img src={robotPng} style={{ width: '80%', height: 'auto' }} alt="robot avatar" />
               </Col>
               <Col span={21} style={{ lineHeight: '100px', fontSize: '40px', textAlign: 'left', }}>
                 <span>小Ai</span>
@@ -122,10 +119,13 @@ export const QaPage = () => {
           </Block>
           <Block padding style={{ padding: '10px', paddingBottom: '10px', textAlign: 'left' }}>
             <Card>
-              {qaAnswer.split('\n').map((v, i) => <div key={i}>{v}</div>)}
+              {qa.message.split('\n').map((v, i) => <div key={i}>{v}</div>)}
             </Card>
           </Block>
-        </>)}
+        </>}
+      </>)}
+
+      {!guessTagShow && qaAnswerLoading && <Block padding><Skeleton /></Block>}
 
       {guessTagShow && <Block padding contentStyle={{ padding: '0 10px', textAlign: 'left' }}>
         <Card title="猜你想问">
