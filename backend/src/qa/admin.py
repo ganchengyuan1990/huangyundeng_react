22541b1853c66a88d8ec42be8706ea56272d43e7
linyh <yanghui@meri.co>
2023-11-12 18:07:28 +0800

    制作更通用的embedding过程

--- backend/src/qa/admin.py
+++ backend/src/qa/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from django.db.models import QuerySet
+from django.db.models import QuerySet, Q
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.schema import Document
+from langchain.vectorstores.qdrant import Qdrant
 
-from src.account.models import Platform, FileUpload
+from src.account.models import Platform
+from src.config import config
 from src.qa.models import HotQuestion
 
 
@@ -19,9 +23,53 @@ class PlatformAdmin(admin.ModelAdmin):
     actions = ['创建qdrant集合']
 
     def 创建qdrant集合(self, request, queryset: QuerySet[Platform]):
-        for item in queryset:
+        for platform in queryset:
+            questions = HotQuestion.objects.filter(Q(platform__isnull=True) | Q(platform=platform)).all()
+            self.message_user(request, f"found {len(questions)} questions")
+            if len(questions) == 0:
+                continue
+            docs = []
+            for question in questions:
+                docs.append(Document(page_content=question.standard_question, metadata={
+                    '地域': question.region,
+                    '大类': question.category_1,
+                    '小类': question.category_2,
+                    '标签': question.tag,
+                    'type': 'question',
+                    'question_type': 1,
+                    'answer': question.standard_answer,
+                }))
+                if type(question.similar_questions) == str and len(question.similar_questions) > 10:
+                    for similar_question in question.similar_questions.split('？ '):
+                        docs.append(Document(page_content=similar_question + '?', metadata={
+                            '地域': question.region,
+                            '大类': question.category_1,
+                            '小类': question.category_2,
+                            '标签': question.tag,
+                            'type': 'question',
+                            'question_type': 2,
+                            'answer': question.standard_answer,
+                        }))
+                docs.append(Document(page_content=question.standard_answer, metadata={
+                    '地域': question.region,
+                    '大类': question.category_1,
+                    '小类': question.category_2,
+                    '标签': question.tag,
+                    'type': 'answer',
+                    'question': question.standard_question,
+                }))
 
-            self.message_user(request, f"{len(df)} records imported successfully.")
+            self.message_user(request, f"create {len(docs)} docs")
+            embeddings = OpenAIEmbeddings()
+            qdrant = Qdrant.from_documents(
+                docs,
+                embeddings,
+                force_recreate=True,
+                url=config['qdrant']['url'],
+                collection_name=platform.qdrant_collection_name,
+            )
+
+            self.message_user(request, f"{len(docs)} records imported to {platform.qdrant_collection_name} successfully.")
 
 
 admin.site.register(HotQuestion, HotQuestionAdmin)
